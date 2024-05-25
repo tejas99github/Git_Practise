cb6f7c4cc44235386cc8623d4ac0e3b8cf2c8959
tejas <bannore99tejas@gmail.com>
2024-05-25 14:32:31 +0530

    sdf

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -8,6 +8,12 @@ import org.junit.Test;
  * Unit test for simple App.
  */
 public class AppTest {
+<<<<<<< Updated upstream
+=======
+    // bug no 101
+    int stash = 50;
+
+>>>>>>> Stashed changes
     /**
      * Rigorous Test :-)
      */
