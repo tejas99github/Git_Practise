fc7aa86a20a3ddbcdac6c2f715df4f275cddc9d5
tejas <bannore99tejas@gmail.com>
2024-05-25 14:31:24 +0530

    demo

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -7,14 +7,15 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
     }
+
+    // bug 102
+    int stashapply = 50;
 }
